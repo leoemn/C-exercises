f4dd4b7bf542ccd3e9552be12c29fc07674761c6
leoemn <91314367+leoemn@users.noreply.github.com>
2023-07-21 18:37:17 +0100

    Update exercise1.7.c

--- exercise1.7.c
+++ exercise1.7.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Function prototypes
 char operator;
 bool isoperator(char operator);
 
@@ -10,42 +11,57 @@ int main(void)
 {
     float a, b;
     
+    // Input the first number
     printf("Enter first number:");
     scanf("%f", &a);
+    
+    // Input the second number
     printf("Enter second number");
     scanf("%f", &b);
     
     do
     {
+        // Input the operator and validate it
         printf("Enter an operator(+, -, *, /)");
-        scanf("%c", &operator);
-    } while (!isoperator(operator));
+        scanf(" %c", &operator); // Note the space before %c to consume any whitespace characters left in the buffer
+        
+    } while (!isoperator(operator)); // Loop until a valid operator is entered
 
+    // Perform the operation based on the operator
     switch (operator)
     {
     case '+':
-        printf("%f + %f = %f", a,b,a+b);
+        printf("%f + %f = %f", a, b, a + b);
         break;
     case '-':
-        printf("%f - %f = %f", a,b,a-b);
+        printf("%f - %f = %f", a, b, a - b);
         break;
     case '*':
-        printf("%f * %f = %f", a,b,a*b);
+        printf("%f * %f = %f", a, b, a * b);
         break;
     case '/':
-        printf("%f / %f = %f", a,b,a/b);
+        if (b == 0)
+        {
+            printf("Cannot divide by zero!");
+        }
+        else
+        {
+            printf("%f / %f = %f", a, b, a / b);
+        }
         break;
     
     default:
-        printf("This is most probably a bug");
+        printf("Most probably a BUG!");
         break;
     }
     
+    return 0;
 }
 
+// Function to check if the given character is a valid operator (+, -, *, /)
 bool isoperator(char operator)
 {
-    if(operator == '+' || operator == '-', operator == '*', operator == '/')
+    if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
     {
         return true;
     }
